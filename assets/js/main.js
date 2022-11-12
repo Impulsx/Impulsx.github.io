0df59bcffd8c65bf27095f4af797460ccf429682
Impulsx <idqdmaster@yahoo.com>
2022-11-12 05:20:11 -0600

    0.01

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ document.addEventListener(
 		particlesJS("particles-js", {
 			particles: {
 				number: { value: 30, density: { enable: !0, value_area: 800 } },
-				color: { value: "#ffffff" },
+				color: { value: "#008080" }, //#ffffff
 				shape: {
 					type: "polygon",
 					stroke: { width: 0, color: "#000000" },
@@ -30,7 +30,7 @@ document.addEventListener(
 				line_linked: {
 					enable: !0,
 					distance: 150,
-					color: "#ffffff",
+					color: "#008080", //#ffffff
 					opacity: 0.4,
 					width: 1,
 				},
